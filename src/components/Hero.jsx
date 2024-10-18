9653903a45d1cb31c849500d2fb7602ca0edb9fc
balajikavi612 <147909825+balajikavi612@users.noreply.github.com>
2024-10-18 16:56:53 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     
-    <div className="min-h-[600px] bg-black/40 "  style={{ backgroundImage: "url(/src/assets/banner.avif)", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center", height: "100%", width: "100%" }}>
+    <div className="min-h-[600px] bg-black/40 "  style={{ backgroundImage: "url(./src/assets/banner.avif)", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center", height: "100%", width: "100%" }}>
          <div className='text-center py-52 bg-black/50'>
         <h1 className="text-2xl font-semibold text-white uppercase">Techbrain Networks</h1>  
         <p className='text-white text-2xl md:text-3xl lg:text-4xl xl:text-6xl 2xl:text-7xl font-bold mt-4'>Advanced Aerospace Solutions for Precision and Performance</p>
